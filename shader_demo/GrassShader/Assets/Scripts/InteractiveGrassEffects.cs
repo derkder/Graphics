9f785d7b6d69641460f26cc35d983624d02ffd13
xuhaoyi <xuhaoyi01@corp.netease.com>
2023-08-01 17:26:50 +0800

    unity shader 草的效果

---
+++ shader_demo/GrassShader/Assets/Scripts/InteractiveGrassEffects.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveGrassEffects : MonoBehaviour
+{
+    [SerializeField]
+    MeshRenderer grassRenderer;
+
+    void Awake()
+    {
+        Material mat = grassRenderer.material;
+
+        Camera cam = GetComponent<Camera>();
+        mat.SetTexture("_EffectRT", cam.targetTexture);
+        mat.SetFloat("_OrthographicCamSize", cam.orthographicSize);
+        mat.SetVector("_CamPos", transform.position);
+    }
+}
---
+++ shader_demo/GrassShader/README.md
@@ -0,0 +1,4 @@
+# Grass Shader
+
+* [《Grass Shader实现风吹草地的效果》传送门>>>](https://zhuanlan.zhihu.com/p/463852264)
+* [《Grass Shader实现可交互的草地效果》传送门>>>](https://zhuanlan.zhihu.com/p/465551866)
